e85afd575d8fb2c9dc991571c92e0ed914cfa684
lucas <lucasmacuna@gmail.com>
2023-04-11 20:02:40 -0300

    add peticion http post

--- JS/correo.js
+++ JS/correo.js
@@ -3,7 +3,25 @@ const form=document.querySelector("#form")
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault()
+    const data = new FormData(e.currentTarget)
    let r=window.confirm("Enviar?")
 
-   r?alert("Enviando..."):alert("Envio cancelado")
+      if(r){
+           
+        const envio= async()=>{
+            var res=await fetch('https://web-vercel-fli1lvdp9-lmacuna.vercel.app/contact',{
+                method:'POST',
+                body:data
+            })
+
+            res=await res.json()
+
+            return console.log(res)
+
+        }
+        envio()
+
+      }else if(!r){
+           alert("cancelado")
+      }
 })
